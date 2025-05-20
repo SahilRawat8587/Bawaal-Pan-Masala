2c5a668c57152a0455010e63397a8ff397d0fab6
Sahil Rawat <sahilr8587@gmail.com>
2025-05-20 12:07:33 +0530

    Fix minimum height of Product component to ensure proper display

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -3,7 +3,7 @@ import ProductAccordion from './components/ProductAccordion'
 
 const Product = () => {
   return (
-    <div className='w-full h-full min-h-screens bg-contain bg-center bg-repeat bg-[url("/public/Bg.avif")]'>
+    <div className='w-full h-full min-h-screen bg-contain bg-center bg-repeat bg-[url("/public/Bg.avif")]'>
         <ProductAccordion />
       
     </div>
